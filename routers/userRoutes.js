cc67c1e9a1aaed5eb481d443756a335a41377242
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-04-12 02:08:25 -0300

    feat(userRouter): create route endpoint /user/id get

--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -5,6 +5,7 @@ const userController = require('../controllers/user/index');
 const middlewares = require('../middlewares/user');
 
 router.post('/', middlewares.createUser, userController.createUser);
+router.get('/:id', middlewares.validToken, userController.findById);
 router.get('/', middlewares.validToken, userController.findAll);
 
 module.exports = router;
