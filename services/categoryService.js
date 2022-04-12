80edbb8fb587f000e0174c7dae9d8d70bf102669
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-04-12 03:08:19 -0300

    feat(categoryService): create function findAll category

--- services/categoryService.js
+++ services/categoryService.js
@@ -5,6 +5,12 @@ const create = async ({ name }) => {
   return { code: 201, payload: category };
 };
 
+const findAll = async () => {
+  const categories = await Category.findAll();
+  return { code: 200, payload: categories };
+};
+
 module.exports = {
   create,
+  findAll,
 };
