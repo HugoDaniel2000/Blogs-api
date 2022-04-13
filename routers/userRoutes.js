ad04789a6ba62ae5b83388916b2e57bd225f2372
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-04-13 17:52:36 -0300

    feat(blogPostRouter): create route endpoint /user/me delete

--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const userController = require('../controllers/user/index');
 const middlewares = require('../middlewares/user');
 
+router.delete('/me', middlewares.validToken, userController.remove);
 router.post('/', middlewares.createUser, userController.createUser);
 router.get('/:id', middlewares.validToken, userController.findById);
 router.get('/', middlewares.validToken, userController.findAll);
