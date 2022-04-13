9e4acf2e85f39c0a8028b4d6f1efe47821715b21
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-04-12 22:09:02 -0300

    feat(blogPostService): create function update post

--- services/blogPostService.js
+++ services/blogPostService.js
@@ -23,14 +23,29 @@ const findById = async (id) => {
     where: { id },
     include: [{ model: User, as: 'user' },
   { model: Category, as: 'categories', through: { attributes: [] } }] });
-
   if (!post) {
     return { code: 404, payload: { message: 'Post does not exist' } };
   }
   return { code: 200, payload: post };
 };
+
+const update = async ({ title, content }, userId, id) => {
+  const post = await BlogPost.findOne({ where: { id } });
+  console.log(post);
+  if (!post || userId !== post.dataValues.userId) {
+    return { code: 401, payload: { message: 'Unauthorized user' } };
+  }
+  await BlogPost.update({ title, content }, { where: { id } });
+  const postUpdated = await BlogPost.findOne({
+    where: { id },
+    attributes: { exclude: ['published', 'updated'] },
+    include: [{ model: Category, as: 'categories', through: { attributes: [] } }] });
+  return { code: 200, payload: postUpdated };
+};
+
 module.exports = {
   create,
   findAll,
   findById,
+  update,
 };
